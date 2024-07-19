bccc5145747bb7c35cb8cbbe5b32cb3810acb1d6
choudharymahipal <mschoudhary81@gmail.com>
2024-07-19 11:25:05 +0530

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  SOCKET_ENDPOINT: "http://localhost:3000",
-  //SOCKET_ENDPOINT:"https://mahi-pointing-poker-api.onrender.com"
+  //SOCKET_ENDPOINT: "http://localhost:3000",
+  SOCKET_ENDPOINT:"https://mahi-pointing-poker-api.onrender.com"
 };
